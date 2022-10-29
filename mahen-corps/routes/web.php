8e0b29fedd3422b8f890839686b73825ea35aa5f
Alginatic <203040094@mail.unpas.ac.id>
2022-10-29 15:31:29 +0700

    rev

---
+++ mahen-corps/routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "Home",
+        'active' => 'categories'
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "name" => "Rizky Mahendra",
+        "email" => "203040094@mail.unpas.ac.id",
+        "image" => "profile.jpg",
+        'active' => 'categories'
+    ]);
+});
+
+
+Route::get('/posts', [PostController::class, 'index']);
+
+// Halaman Single Post
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function () {
+    return view('categories', [
+       'title' => 'Post Categories',
+       'active' => 'categories',
+       'categories' =>Category::all()
+    ]);
+});
+
+
